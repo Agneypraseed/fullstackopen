d561292d5c7bc96bb4f3c5b8328ada6ed3e4eae9
Agney Praseed <agney.praseed@publicissapient.com>
2023-09-26 01:22:18 +0530

    2.9: The Phonebook Step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNunber] = useState("");
+  const [filterFlag, setFilterFlag] = useState(false);
+  const [filterdPersons, setFilteredPersons] = useState([{}]);
 
   let saveFlag = true;
 
@@ -38,9 +43,36 @@ const App = () => {
     setNewNunber(e.target.value);
   };
 
+  const handleFilter = (e) => {
+    const filterValue = e.target.value.toLowerCase().trim();
+    setFilterFlag(filterValue !== "");
+    setFilteredPersons(
+      persons.filter((person) => {
+        return person.name.toLowerCase().includes(filterValue);
+        //  && person.name.toLowerCase().startsWith(filterValue);
+      })
+    );
+  };
+
+  const Persons = ({ persons }) => {
+    return (
+      <>
+        {persons.map((person) => (
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        ))}
+      </>
+    );
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleFilter} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleName} />
@@ -53,11 +85,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      {filterFlag ? (
+        <Persons persons={filterdPersons} />
+      ) : (
+        <Persons persons={persons} />
+      )}
     </div>
   );
 };
